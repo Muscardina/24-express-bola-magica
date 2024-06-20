7170522b467f4fb6a0143b52be00460440a38188
Muscardina <vergonzalezsainz@gmail.com>
2024-06-20 12:02:48 +0200

    Al fin funciona

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//app.get('/magia', (req, res) => {
+//res.send('Hello World!')
+//})
+
+const respuestas = ["It is certain", "Reply hazy", "Try again", "Don’t count on it", "It is decidedly so", "Ask again later", "My reply is no",
+"Without a doubt", "Better not tell you now", "My sources say no", "Yes definitely", "Cannot predict now", "Outlook not so good", "You may rely on it",
+"Concentrate and ask again", "Very doubtful", "As I see it- yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"];
+
+app.get("/magia", (req, res) => {
+    pregunta = respuestas[Math.floor(Math.random() * respuestas.length)];
+    res.send(pregunta);
 })
 
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Esto chufla en el puerto ${port}`)
 })
+
